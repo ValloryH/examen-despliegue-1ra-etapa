40f5515c73a30c8e3f87188b39b73ab22f3b118b
JimePy <jimenalopezpy@gmail.com>
2025-06-17 14:35:39 -0400

    cambio de credenciales en la base de datos

--- tu_aplicación_web/config.php
+++ tu_aplicación_web/config.php
@@ -1,9 +1,9 @@
 <?php
 // config.php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'u178928053_jime_hugo'); // usuario de MySQL
-define('DB_PASSWORD', '@1#f7yeS');     // contraseña de MySQL
-define('DB_NAME', 'u178928053_examen_jh');
+define('DB_USERNAME', 'root'); // Usuario por defecto de XAMPP/WAMP
+define('DB_PASSWORD', '');     // Contraseña vacía por defecto de XAMPP/WAMP
+define('DB_NAME', 'u178928053_examen_jh'); 
 
 // Intentar conectar a la base de datos MySQL
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
