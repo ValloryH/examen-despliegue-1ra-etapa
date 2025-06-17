8dcf9101502846c301592c78ed893e377a491707
JimePy <jimenalopezpy@gmail.com>
2025-06-17 17:13:12 -0400

    Nuevo archivo php

---
+++ tu_aplicación_web/bd.php
@@ -0,0 +1,33 @@
+<?php
+/*Base de datos:
+- Contraseña: @1#f7yeS
+- Nombre BD: u178928053_examen_jh
+- Usuario BD: u178928053_jime_hugo */
+
+$conexion= new mysqli("localhost", "u178928053_jime_hugo ", "@1#f7yeS", "u178928053_examen_jh");
+
+
+$sql = "CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(50) NOT NULL UNIQUE,
+    `email` VARCHAR(100) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL, 
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+";
+
+$conexion->query(query: $sql);
+
+$sql = "CREATE TABLE IF NOT EXISTS `posts` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `content` TEXT NOT NULL,
+    `image_path` VARCHAR(255) NULL, -- Ruta a la imagen (opcional)
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
+);
+";
+
+$conexion->query(query: $sql);
